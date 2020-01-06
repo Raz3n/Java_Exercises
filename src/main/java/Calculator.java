22b07c6d0244c7c686396f164dd9769c8c4db482
Nelson <nbritob@hotmail.com>
2020-01-06 14:56:20 +0000

    addidtion function added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,16 @@
 public class Calculator {
+
+    private double firstNum;
+    private double secondNum;
+
+    public Calculator(double firstNum, double secondNum){
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+    }
+
+    public double add(){
+        return this.firstNum + this.secondNum;
+    }
+
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator;
+    @Before
+    public void before(){
+        calculator = new Calculator(25.0, 5.0);
+    }
+
+    @Test
+    public void add(){
+        assertEquals(30.0, calculator.add(), 0.01);
+    }
 
 }
