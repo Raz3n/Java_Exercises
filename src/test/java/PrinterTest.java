2eb55d59f54a76d0e251af6a89c112153ee907ce
Nelson <nbritob@hotmail.com>
2020-01-06 15:49:01 +0000

    MVP Done, Extensions finished

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -8,11 +8,47 @@ public class PrinterTest {
 
     @Before
     public void before(){
-        printer = new Printer(20);
+        printer = new Printer(20, 20);
     }
 
     @Test
     public void getSheets(){
         assertEquals(20, printer.getSheets());
     }
+
+    @Test
+    public void willPrintIfPaperAvailable(){
+        printer.print(5, 4);
+        assertEquals(0, printer.getSheets());
+    }
+
+    @Test
+    public void wontPrintIfPaperUnavailable(){
+        printer.print(5, 5);
+        assertEquals(20, printer.getSheets());
+    }
+
+    @Test
+    public void getToner(){
+        assertEquals(20, printer.getToner());
+    }
+
+    @Test
+    public void willPrintIfTonerAvailable(){
+        printer.print(5, 4);
+        assertEquals(0, printer.getToner());
+    }
+
+    @Test
+    public void wontPrintIfTonerUnavailable(){
+        printer.print(5, 5);
+        assertEquals(20, printer.getSheets());
+    }
+
+
+//    @Test
+//    public void willPrintIfPaperAvailable2(){
+//        printer.print(4, 4);
+//        assertEquals(4, printer.getSheets());
+//    }
 }
